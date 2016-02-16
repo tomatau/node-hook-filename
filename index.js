bf60a44b5769c3580a3b9121e3c4eeb764fa2e89
Noah Ark <nowarkdev@gmail.com>
2016-02-16 15:37:37 +0000

    RP, MS: Allows extra argument to be used as the module when it is imported

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ var Module = require('module');
 
 var originalResolve = Module._resolveFilename;
 
-module.exports = function(extensions) {
+module.exports = function(extensions, override) {
   extensions.forEach(function(ext) {
     Module._extensions[ext] = function(module, filename) {
       module.exports = filename;
     }
-  })
+  });
 
   var regexes = extensions.map(function(ext){ return new RegExp(ext, '') })
 
   Module._resolveFilename = function(request, parent) {
     if (regexes.some(function(regex){ return request.match(regex) })) {
+      if (override) {
+        return originalResolve.call(this, override, parent);
+      }
       return request;
     }
     return originalResolve.call(this, request, parent);
   }
-}
+};
