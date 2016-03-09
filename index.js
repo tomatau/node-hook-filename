7d03fee33c539298eb68d7181f097240e78dceb1
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-03-09 20:42:38 +0000

    added eslint rules

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const Module = require('module');
-const originalLoad = Module._load;
+const Module = require('module')
+const originalLoad = Module._load
 
 module.exports = function(extensions, override) {
-  override = override ? override : (r) => r;
+  override = override ? override : (r) => r
 
   extensions.forEach((ext) => {
     Module._extensions[ext] = (module, filename) => {
-      module.exports = filename;
+      module.exports = filename
     }
-  });
+  })
 
   Module._load = function(request, parent, isMain) {
     const requestMatchesExt = (ext) => request.includes(ext)
     if (extensions.some(requestMatchesExt)) {
-      return override(request);
+      return override(request)
     }
-    return originalLoad.call(this, request, parent, isMain);
+    return originalLoad.call(this, request, parent, isMain)
   }
-};
+}
